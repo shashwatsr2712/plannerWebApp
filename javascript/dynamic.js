aa581074e4a42cbc9daf46fbc2c928e231f0e97f
shashwatsr2712 <44287108+shashwatsr2712@users.noreply.github.com>
2018-12-13 20:31:59 +0530

    Added Pending Tasks Count

--- javascript/dynamic.js
+++ javascript/dynamic.js
@@ -8,10 +8,23 @@ function conf(){
 var appmod=angular.module("myApp",[]);
 appmod.controller("myController",function($scope){
 	$scope.todos=[{text:'The Tasks will appear like this',done:false}];
+	$scope.pendingCount=0;
+	$scope.countPending=function(){
+		$scope.doneCount=0;
+		for(i=0;i<$scope.todos.length;i++){
+			if($scope.todos[i].done==true)
+				$scope.doneCount+=1;
+		}
+		$scope.pendingCount=$scope.todos.length-$scope.doneCount;
+	};
 	$scope.addToDo=function(){
 		if($scope.todoText){
-			if($scope.todos[0].text=='The Tasks will appear like this')
+			if($scope.todos[0].text=='The Tasks will appear like this'){
 				$scope.todos=[];
+				$scope.pendingCount=1;
+			}
+			else
+				$scope.pendingCount+=1;
 			$scope.todos.push({text:$scope.todoText,done:false});
 			$scope.todoText='';
 		}
@@ -19,5 +32,14 @@ appmod.controller("myController",function($scope){
 	$scope.delToDo=function(){
 		$scope.todos=[];
 		$scope.todos=[{text:'The Tasks will appear like this',done:false}];
+		$scope.pendingCount=0;
+	};
+	$scope.countPending=function(){
+		$scope.doneCount=0;
+		for(i=0;i<$scope.todos.length;i++){
+			if($scope.todos[i].done==true)
+				$scope.doneCount+=1;
+		}
+		$scope.pendingCount=$scope.todos.length-$scope.doneCount;
 	};
 });
