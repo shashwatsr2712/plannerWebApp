721e7e19ce59e35ddd772d292959e6b9be1338f6
shashwatsr2712 <shashwatsr2712@gmail.com>
2019-06-21 19:03:35 +0530

    Update ajax2.php

--- ajax2.php
+++ ajax2.php
@@ -6,11 +6,34 @@ if(!($_POST['todo1text']==='The Tasks will appear like this')){
 		$str2="todo".$i."done";
 		$plan=mysqli_real_escape_string($con,$_POST[$str1]);
 		$status=mysqli_real_escape_string($con,$_POST[$str2]);
-		if($status=='true'){
-		$query="INSERT INTO users_plans(user_id,plan,status) VALUES('".$_SESSION['id']."','$plan','Done')";}
+		
+		$queryx="SELECT plan,status FROM users_plans WHERE user_id='".$_SESSION['id']."'";
+		$run_queryx=mysqli_query($con,$queryx) or die(mysqli_error($con));
+		$num=mysqli_num_rows($run_queryx);
+		$s='';$flag=0;$st='';
+		while($row=mysqli_fetch_array($run_queryx)){
+			$s=$row['plan'];
+			$st=$row['status'];
+			if($s===$plan){
+				$flag=1;
+				break;
+			}
+		}
+		
+		if($flag===1){
+			if($status=='true'){
+			$query="UPDATE users_plans SET status='Done' WHERE plan='$s'";}
+			else{
+			$query="UPDATE users_plans SET status='Pending' WHERE plan='$s'";}
+			$run_query=mysqli_query($con,$query) or die(mysqli_error($con));
+		}
 		else{
-		$query="INSERT INTO users_plans(user_id,plan,status) VALUES('".$_SESSION['id']."','$plan','Pending')";}
-		$run_query=mysqli_query($con,$query) or die(mysqli_error($con));
+			if($status=='true'){
+			$query="INSERT INTO users_plans(user_id,plan,status) VALUES('".$_SESSION['id']."','$plan','Done')";}
+			else{
+			$query="INSERT INTO users_plans(user_id,plan,status) VALUES('".$_SESSION['id']."','$plan','Pending')";}
+			$run_query=mysqli_query($con,$query) or die(mysqli_error($con));
+		}
 	}
 }
 ?>
